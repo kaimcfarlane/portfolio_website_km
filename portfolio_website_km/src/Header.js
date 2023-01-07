adfd87fbb2f8e1172578da4b5c7515f60b216a01
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-01-07 15:21:56 -0500

    Added CSS classes to access both ids and classes. Note: ::before and ::after css was not changing

--- portfolio_website_km/src/Header.js
+++ portfolio_website_km/src/Header.js
@@ -1,7 +1,7 @@
 import React, {useRef} from 'react'
 // import './Header.css'
 import styles from "./Header.module.css"
-import "./Header.module.css"
+import "./burgerMenu.css"
 
 export default function Header() {
     //We are implementing button here
@@ -10,8 +10,13 @@ export default function Header() {
     //as well as solve cuyrrent ones
 
     const burgerMenu = useRef();
+    const burgerMenuLeft = useRef();
+    const burgerMenuRight = useRef();
+
     function burgerMenuClick() {
         burgerMenu.current.classList.toggle('open');
+        burgerMenuLeft.current.classList.toggle('open');
+        burgerMenuRight.current.classList.toggle('open');
         console.log("burgerMenu Clicked!");
     } 
 
@@ -21,13 +26,13 @@ export default function Header() {
     <>
     <section id={styles.header}>
     <div>This is the header</div>
-        <div ref={burgerMenu} className="menu btn11" data-menu="11" onClick={burgerMenuClick}>
-            <div className="icon-left"></div>
-            <div className="icon-right"></div>
+        <div ref={burgerMenu} className={[styles.menu, styles.btn11].join(' ')} data-menu="11" onClick={burgerMenuClick}>
+            <div ref={burgerMenuLeft} className={styles["icon-left"]}></div>
+            <div ref={burgerMenuRight} className={styles["icon-right"]}></div>
         </div>
         <div id='menuContent'>
             <ul id='dropdown'>
-                <li className='headerItem'><a href='#'>ABOUT ME</a></li>
+                <li className={styles.headerItem}><a href='#'>ABOUT ME</a></li>
                 <li className='headerItem'><a href='#'>SKILLS</a></li>
                 <li className='headerItem'><a href='#'>PROJECTS</a></li>
                 <li className='headerItem'><a href='#'>CONTACT</a></li>
