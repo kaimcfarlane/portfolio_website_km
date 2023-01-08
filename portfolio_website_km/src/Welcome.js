3a601307be71faaa91e23b0dee3f4a1fa5c7d9ec
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-01-07 23:23:43 -0500

    Added pictures and added main picture to landing page with purple highlight styling

--- portfolio_website_km/src/Welcome.js
+++ portfolio_website_km/src/Welcome.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './Welcome.module.css'
+import imageC from './selfPictures/selfie2.jpg'
+import imageS from './KaiMcFarlaneSeniorPicture2.jpeg'
 
 export default function Welcome() {
   return (
+    <section id={styles.welcomeAll}>
     <div id={styles.welcomeContent}>
         <h2 id={styles.subtitle}>Hey I'm,</h2>
         <h1 id={styles.title}>Kai McFarlane</h1>
+        <p2 id={styles.paragraph1}>Full-Stack Developer - CS Student</p2>
+        <p1 id={styles.paragraph2}>I'm a Software Developer that loves to create exciting projects through programming. I'm currently working in Web-Development and pursuing a degree in computer science from the University of Florida</p1>
     </div>
+    <div>
+        <img id={styles.profilePic} alt='Logo' src={imageC}></img>
+    </div>
+    </section>
   )
 }
