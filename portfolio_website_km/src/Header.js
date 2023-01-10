408661a2c52e526fb9e60001c9d39fbd83332040
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-01-09 20:06:16 -0500

    Fixed Scroll bar appearing by using right to place burger menu items to right of page instead of left

--- portfolio_website_km/src/Header.js
+++ portfolio_website_km/src/Header.js
@@ -40,7 +40,9 @@ export default function Header() {
             menuContent.current.style.animation = "slideLeft"
             setTimeout(() => {
                 // menuContent.current.style.opacity = "100%"
-                menuContent.current.style.left = "86.5%"
+                // menuContent.current.style.left = "86.5%"
+                // menuContent.current.style.left = "0";
+                menuContent.current.style.right = "0";
                 burgerMenu.current.style.backgroundColor = "#a7a7fa";
             },100)
             // burgerMenu.current.style.backgroundColor = "#7a61f0d9";
@@ -50,7 +52,8 @@ export default function Header() {
             menuContent.current.style.animation = "slideRight"
             // burgerMenu.current.style.animation = "slideBurgerRight";
             setTimeout(() => {
-                menuContent.current.style.left = "100%"
+                // menuContent.current.style.left = "100%"
+                menuContent.current.style.right = "-285px";
                 setTimeout(() => {
                     menuContent.current.style.display = "none"
                 },600)
@@ -89,3 +92,26 @@ export default function Header() {
     </>
   )
 }
+
+
+
+//Our slide menu is overflowing because it is only partially coming out left
+//To fix make left 100% and format so it looks same as before
+//Next add parrallax for on mouse move for profile picture 
+//Then add the typing for hey I'm in different languages
+//Add dark light mode slector next to burger menu, try just balck white selector
+//Add zoom in/zoom out hover effect for logo
+//Add linkedin and github and resume if we can find logo into burger menu
+//Add load in animations for all things on the screen with the mouse coming in last probably
+//just add everthing come in from below with opacity 0 to 100 with header first then center stuff then mouse icon animation
+//just barely lower than orginal location too
+
+
+//For anything below full screen
+//82.85 for line to no scroll bar but line show
+//83.1 For minimum scroll bar
+
+//For full screen
+//85.4% With right line
+//
+
