69873059bdbf9686ac4492a2680c2c3bba0930f2
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-01-09 21:42:13 -0500

    Added Parallax to all middle landing page content

--- portfolio_website_km/src/Welcome.js
+++ portfolio_website_km/src/Welcome.js
@@ -6,12 +6,28 @@ import imageS from './KaiMcFarlaneSeniorPicture2.jpeg'
 export default function Welcome() {
 
     const profilePic = useRef();
+    const subtitle = useRef();
+    const title = useRef();
+    const paragraph1 = useRef();
+    const paragraph2 = useRef();
+
     document.addEventListener('mousemove', parrallax);
     function parrallax(e) {
         const speed = 4;
+        const speed2 = -3;
+        const speed3 = 2;
+        const speed4 = -1;
         const x = (window.innerWidth - e.pageX*speed)/100;
+        const x2 = (window.innerWidth - e.pageX*speed2)/100;
+        const x3 = (window.innerWidth - e.pageX*speed3)/100;
+        const x4 = (window.innerWidth - e.pageX*speed4)/100;
         const y = (window.innerHeight - e.pageY*speed)/100;
+        
         profilePic.current.style.transform = "translateX(" + x + "px)" + "translateY(" + y + "px)";
+        subtitle.current.style.transform = "translateX(" + x2 + "px)" + "translateY(" + y + "px)";
+        title.current.style.transform = "translateX(" + x3 + "px)" + "translateY(" + y + "px)";
+        paragraph1.current.style.transform = "translateX(" + x + "px)" + "translateY(" + y + "px)";
+        paragraph2.current.style.transform = "translateX(" + x + "px)" + "translateY(" + y + "px)";
         // console.log("Mouse Moved Bro");
         // console.log("Page X speed is " + e.pageX);
         // console.log("X value: " + x);
@@ -21,10 +37,10 @@ export default function Welcome() {
     <>
     <section id={styles.welcomeAll}>
     <div id={styles.welcomeContent}>
-        <h2 id={styles.subtitle}>Hey I'm,</h2>
-        <h1 id={styles.title}>Kai McFarlane</h1>
-        <p2 id={styles.paragraph1}>Full-Stack Developer - CS Student</p2>
-        <p1 id={styles.paragraph2}>I am a software developer driven by a love for creating exciting and impactful projects. I'm currently exploring Web-Development and pursuing a degree in computer science from the University of Florida.</p1>
+        <h2 ref={subtitle} id={styles.subtitle}>Hey I'm,</h2>
+        <h1 ref={title} id={styles.title}>Kai McFarlane</h1>
+        <p2 ref={paragraph1} id={styles.paragraph1}>Full-Stack Developer - CS Student</p2>
+        <p1 ref={paragraph2} id={styles.paragraph2}>I am a software developer driven by a love for creating exciting and impactful projects. I'm currently exploring Web-Development and pursuing a degree in computer science from the University of Florida.</p1>
         {/* I'm a Software Developer that loves to create exciting projects through code. */}
     </div>
     <div>
