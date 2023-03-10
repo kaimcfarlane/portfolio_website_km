5b69e58a0affe483d2af234eb14fb98fd1cc87ae
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-03-10 15:28:29 -0500

    Added the rest of the skill Logos

--- portfolio_website_km/src/Skills.js
+++ portfolio_website_km/src/Skills.js
@@ -7,6 +7,12 @@ import java_Logo from './javaLogo.png'
 import react_Logo from './reactLogo.png'
 import typescript_Logo from './typescriptLogo.png'
 import cSharp_Logo from './cSharp_Logo.png'
+import express_Logo from './expressLogo.png'
+import mui_Logo from './materialuiLogo.png'
+import node_Logo from './nodeLogo.png'
+import mongodb_Logo from './mongodb_Logo.png'
+import api_Logo from './apiLogo.png'
+import git_Logo from './gitLogo.png'
 
 export default function skillsPage() {
   return (
@@ -15,7 +21,7 @@ export default function skillsPage() {
       <div id='skillsText'>
         <h1>I'm</h1>
         <h1>Experienced</h1>
-        <p>- 3 Years Experience -</p>
+        <p>- 3 Years of Code -</p>
       </div>
       <div id='skillsContent'>
         <div>
@@ -25,7 +31,6 @@ export default function skillsPage() {
             <img className='icon' alt='languageIcon' src={css_Icon}></img>
             <img className='icon' alt='languageIcon' src={JS_Icon}></img>
             <img className='icon' id='typescript_Logo' alt='languageIcon' src={typescript_Logo}></img>
-            <img className='icon' id='react_Logo' alt='languageIcon' src={react_Logo}></img>
             <img className='icon' id='java_Logo' alt='languageIcon' src={java_Logo}></img>
             <img className='icon' id='cSharp_Logo' alt='languageIcon' src={cSharp_Logo}></img>
           </div>
@@ -33,17 +38,18 @@ export default function skillsPage() {
         <div>
         <h1>Libraries</h1>
           <div className='imageContainer'>
-            <img className='icon' alt='libraryIcon' src={JS_Icon}></img>
-            <img className='icon' alt='libraryIcon' src={JS_Icon}></img>
-            <img className='icon' alt='libraryIcon' src={JS_Icon}></img>
+            <img className='icon' id='react_Logo' alt='libraryIcon' src={react_Logo}></img>
+            <img className='icon' alt='libraryIcon' src={express_Logo}></img>
+            <img className='icon' alt='libraryIcon' src={mui_Logo}></img>
+            <img className='icon' alt='libraryIcon' src={node_Logo}></img>
           </div>
         </div>
         <div>
         <h1>Learned</h1>
           <div className='imageContainer'>
-            <img className='icon' alt='learnedIcon' src={JS_Icon}></img>
-            <img className='icon' alt='learnedIcon' src={JS_Icon}></img>
-            <img className='icon' alt='learnedIcon' src={JS_Icon}></img>
+            <img className='icon' alt='learnedIcon' src={mongodb_Logo}></img>
+            <img className='icon' alt='learnedIcon' src={api_Logo}></img>
+            <img className='icon' alt='learnedIcon' src={git_Logo}></img>
           </div>
         </div>
       </div>
