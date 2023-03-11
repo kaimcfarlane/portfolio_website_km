e9f8d1ea7d1f4f06dd74ce889ee25f8dc4012121
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-03-11 14:25:58 -0500

    Added images and new design for innovation/achievments page

--- portfolio_website_km/src/Achieve.js
+++ portfolio_website_km/src/Achieve.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './achieve.css'
 import purple_Logo from './purpleawardLogo.png'
+import kiwi_WebsitePic from './kiwiWebsitePic.png'
+import modsvr_Pic from './modsvr_Pic.png'
+import modstank_Pic from './modstankPic.png'
+import ufit_Pic from './ufitPic.png'
 
 
 export default function Achieve() {
@@ -10,6 +14,26 @@ export default function Achieve() {
             <h1>I'm an Innovator and an Achiever</h1>
             <img alt='' src=''></img>
         </div>
+        <div id='workContainer'>
+                <div className='job' id='job1'>
+                    <h1 className='jobh1'>Website Developer and Founder</h1>
+                    <h2 className='jobh2'>Kiwi-El-Gato</h2>
+                    <img id='kiwi_WebsitePic' src={kiwi_WebsitePic} alt=''></img>
+                    <p className='jobDesc'>Developed educational website through digital art, designs, animations, and code</p>
+                </div>
+                <div className='job' id='job2'>
+                    <h1 className='jobh1'>Lead Software Devloper Intern</h1>
+                    <h2 className='jobh2'>Musuem of Discovery and Science</h2>
+                    <img id='modsvr_Pic' src={modsvr_Pic} alt=''></img>
+                    <p className='jobDesc'>Programmed virtual reality images and animations with C# and Unity to explain natural phenomena</p>
+                </div>
+                <div className='job' id='job3'>
+                    <h1 className='jobh1'>Website Application Developer Intern</h1>
+                    <h2 className='jobh2'>University of Florida Information Technology</h2>
+                    <img id='ufit_Pic' src={ufit_Pic} alt=''></img>
+                    <p className='jobDesc'>Redesigning and implementing new cards feature for the personal UF Workspace with javascript frameworks</p>
+                </div>
+            </div>
         <section id='achievePageContainer'>
             <div id='awardsContainer'>
                 <div className='award'>
@@ -27,11 +51,6 @@ export default function Achieve() {
                     <p className='awardText1'>Outstanding Senior Leader Award</p>
                     <p className='awardText2'>-Leadership through Code-</p>
                 </div>
-            </div>
-            <div id='workContainer'>
-                <div className='job'></div>
-                <div className='job'></div>
-                <div className='job'></div>
             </div>
         </section>
     </>
