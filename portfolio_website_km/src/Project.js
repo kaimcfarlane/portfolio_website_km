f3e19ac0cee2c35f993469affc93f1819d94f227
kaimcfarlane <kaimcfarlane43@gmail.com>
2023-03-14 09:07:58 -0400

    Added styling to project boxes and added most porject images

--- portfolio_website_km/src/Project.js
+++ portfolio_website_km/src/Project.js
@@ -4,7 +4,7 @@ import './project.css'
 export default function Project({info}) {
 
   const projectImg = useRef()
-  const projectTitle = useRef()
+  const proTitle = useRef()
   const tool1 = useRef()
   const tool2 = useRef()
   const tool3 = useRef()
@@ -12,7 +12,7 @@ export default function Project({info}) {
   return (
     <div id='project'>
             <img ref={projectImg} src={info.img} alt='' id='projectImg'></img>
-            <h1 ref={projectTitle} id='projectTitle' >{info.title}</h1>
+            <h1 ref={proTitle} id='proTitle' >{info.title}</h1>
             <div id='skillsBox'>
               <p ref={tool1} className='tool' id='tool1'>{info.tool1}</p>
               <p ref={tool2} className='tool' id='tool2'>{info.tool2}</p>
--- portfolio_website_km/src/Projects.js
+++ portfolio_website_km/src/Projects.js
@@ -2,6 +2,12 @@ import {React, useRef} from 'react'
 import Project from './Project'
 import tankPicture from './modstankPic.png'
 import recipeImg1 from './recipeatImg1.png'
+import reactTSImg1 from './reacttsImg1.png'
+import reactTSImg2 from './reacttsImg2.png'
+import reactTSImg3 from './reacttsImg3.png'
+import kiwiImg from './kiwiWebsitePic.png'
+import weatherappImg from './weatherappImg1.png'
+import kiwipongimg from './kiwipongImg.png'
 import './Projects.css'
 
 export default function Projects({DLObj}) {
@@ -16,12 +22,12 @@ export default function Projects({DLObj}) {
   const projectImg1 = useRef()
   const projectTitle1 = useRef()
 
-  const info1 = {img: recipeImg1, title: 'Recipeat', tool1: 'TypeScript', tool2: 'Node.js', tool3: 'MongoDB'}
-  const info2 = {img: recipeImg1, title: 'MyClimi', tool1: 'JavaScript', tool2: 'KeyFrames', tool3: 'RestApi'}
-  const info3 = {img: recipeImg1, title: 'ReactTS', tool1: 'JavaScript', tool2: 'BootStrap', tool3: 'Chart.js'}
-  const info4 = {img: recipeImg1, title: 'Kiwi-El-Gato', tool1: 'JavaScript', tool2: 'JotForm', tool3: 'WordPress'}
-  const info5 = {img: recipeImg1, title: 'Kiwi Pong', tool1: 'C#', tool2: 'Unity', tool3: '2D-Modeling'}
-  const info6 = {img: recipeImg1, title: 'Calculator', tool1: 'C#', tool2: '2D-Arrays', tool3: 'OOP'}
+  const info1 = {img: recipeImg1, title: 'Recipeat', tool1: '//TypeScript', tool2: '//Node.js', tool3: '//MongoDB'}
+  const info2 = {img: weatherappImg, title: 'MyClimi', tool1: '//JavaScript', tool2: '//KeyFrames', tool3: '//RestApi'}
+  const info3 = {img: reactTSImg1, title: 'ReactTS', tool1: '//JavaScript', tool2: '//BootStrap', tool3: '//Chart.js'}
+  const info4 = {img: kiwiImg, title: 'Kiwi-El-Gato', tool1: '//JavaScript', tool2: '//JotForm', tool3: '//WordPress'}
+  const info5 = {img: kiwipongimg, title: 'Kiwi Pong', tool1: '//C#', tool2: '//Unity', tool3: '//2D-Modeling'}
+  const info6 = {img: recipeImg1, title: 'Calculator', tool1: '//C#', tool2: '//2D-Arrays', tool3: '//OOP'}
   
 
   return (
@@ -33,7 +39,7 @@ export default function Projects({DLObj}) {
       <section ref={projectContainer} id='projectContainer'>
         <div className='projectSubBox' ref={topProjects} id='topProjects'>
               <Project info={info1}/>
-              <Project info={info2}/>
+              <Project  info={info2}/>
               <Project info={info3}/>
         </div>
         <div className='projectSubBox' ref={botProjects} id='botProjects'>
